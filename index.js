a799a3b734799481f4feb28da452255945590336
mikebcbc <mikebconstantino@gmail.com>
2017-05-26 15:11:24 -0400

    Added prepend functionality

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-// your code here! 
-// and remember you'll need to link to this file
-// in a <script> element in index.html
+$('#js-shopping-list-form').submit(function(e) {
+	e.preventDefault();
+	$('.shopping-list').prepend(
+		'<li> \
+        <span class="shopping-item">' + $('#shopping-list-entry').val() + '</span> \
+        <div class="shopping-item-controls"> \
+          <button class="shopping-item-toggle"> \
+            <span class="button-label">check</span> \
+          </button> \
+          <button class="shopping-item-delete"> \
+            <span class="button-label">delete</span> \
+          </button> \
+        </div> \
+      </li>'
+		)
+});
